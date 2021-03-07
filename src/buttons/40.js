0a3647c5a2e04722294ff0a83ff6180ea40cc5f7
Jack Latimer <jacklatimer03@gmail.com>
2021-03-07 10:43:54 +0000

    v2.0

--- src/buttons/40.js
+++ src/buttons/40.js
@@ -1,24 +1,34 @@
-import React from 'react';
-import './style.css';
-
+import React from "react";
+import "./style.css";
 
 function App() {
-    return (
-        <div id="viewer">
-            <div className="f416">
-                <h1 className="buttonh1">Button#40</h1>
-                <div className="bn40div">
-                    <a className="bn40" href="/buttons/40">Button</a>
-                </div>
-                <h2 className="h2source">Compatibility: <img alt="firefox" src="https://img.icons8.com/nolan/45/firefox.png" /><img alt="chrome" src="https://img.icons8.com/nolan/45/chrome.png" /><img alt="safari" src="https://img.icons8.com/nolan/45/safari.png" /><img alt="ie" src="https://img.icons8.com/nolan/45/internet-explorer.png" /></h2>
-                <h2 className="h2source">Source: <a href="https://github.com/r1" className="avis" target="_blank" rel="noopener noreferrer">github.com/r1</a></h2>
-                <div>
-                    <pre className="prettyprint">{`<div className="bn40div">
+  return (
+    <div id="viewer">
+      <div className="f416">
+        <h1 className="buttonh1">Button#40</h1>
+        <div className="bn40div">
+          <a className="bn40" href="/buttons/40">
+            Button
+          </a>
+        </div>
+        <h2 className="h2source">
+          Creator:{" "}
+          <a
+            href="https://github.com/r1"
+            className="avis"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            github.com/r1
+          </a>
+        </h2>
+        <div>
+          <pre className="prettyprint">{`<div className="bn40div">
     <a className="bn40" href="/">Button</a>
 </div>`}</pre>
-                </div>
-                <div>
-                    <pre className="prettyprint">{`.bn40 {
+        </div>
+        <div>
+          <pre className="prettyprint">{`.bn40 {
   border-radius: 4px;
   font-weight: 600;
   font-size: large;
@@ -40,10 +50,10 @@ function App() {
 .bn40div {
   display: flex;
 }`}</pre>
-                </div>
-            </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default App;
