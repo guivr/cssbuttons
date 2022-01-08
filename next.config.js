6583f61458d235f1e7130fe1cf03e978e6cb61d3
Jack Latimer <jacklatimer03@gmail.com>
2022-01-08 00:07:53 +0000

    done(ui): changed design & upgraded packages

--- next.config.js
+++ next.config.js
@@ -1,3 +1,9 @@
 module.exports = {
-  reactStrictMode: true,
-}
+  env: {
+    REACT_APP_GA_ENV: process.env.REACT_APP_GA_ENV,
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) config.resolve.fallback.fs = false;
+    return config;
+  },
+};
